5ca283ff947fc9dd840d1bb9918fdb4905f478c9
Illia Ananich <illia.ananich@gmail.com>
2017-11-12 19:34:46 +0200

    upgrade (GSpreadRow), refactor (GSpreadItemExporter):
    * add `Row` ABClass and add `BackupGSpreadRow`
    * add `BaseGSpreadWriter` - it handles the process of writing ArticleItem to Google Sheet
    * `GSpreadItemExporter` now uses writers

--- scrapy_ntk/storage.py
+++ scrapy_ntk/storage.py
@@ -1,5 +1,7 @@
 import logging
+import abc
 from datetime import datetime
+from typing import List, Tuple
 
 import gspread
 import scrapy
@@ -7,7 +9,7 @@ from oauth2client.service_account import \
     ServiceAccountCredentials as Credentials
 
 from . import config
-from .item import ArticleItem, DATE
+from .item import ArticleItem, DATE, URL
 
 cfg = config.cfg
 logger = logging.getLogger(__name__)
@@ -50,22 +52,16 @@ class GSpreadMaster:
         return self._secret_file_name
 
 
-class GSpreadRow:
+class Row(abc.ABC):
     """ Place to configure fields order in a table"""
 
     empty_cell = '- - -'
 
     def __init__(self, item: ArticleItem or dict = None, **fields):
-        self.columns_order = cfg.columns
-
         if item is not None:
             self.item_dict = dict(item)
         else:
             self.item_dict = fields
-
-        for k in self.item_dict:
-            if k not in self.columns_order:
-                logger.warning(f'`{k} is not supported for exporting into Google Sheet.')
 
         self.serialized = self.serialize(self.item_dict)
 
@@ -88,5 +84,90 @@ class GSpreadRow:
             yield self.serialized[column]
 
     @classmethod
-    def to_tuple(cls, **kwargs):
+    def to_tuple(cls, **kwargs) -> tuple:
         return tuple(cls(**kwargs))
+
+    @property
+    @abc.abstractmethod
+    def columns_order(self) -> tuple:
+        pass
+
+
+class GSpreadRow(Row):
+
+    @property
+    def columns_order(self):
+        return cfg.columns
+
+
+class BackupGSpreadRow(Row):
+
+    @property
+    def columns_order(self):
+        return [DATE, URL]
+
+
+class BaseGSpreadWriter(abc.ABC):
+
+    def __init__(self, worksheet: gspread.Worksheet=None):
+        self._worksheet = worksheet
+
+    @property
+    @abc.abstractmethod
+    def Row(self) -> Row:
+        pass
+
+    @property
+    def disabled(self):
+        return self._worksheet is None
+
+    def write(self, *items: List[ArticleItem]):
+        if self.disabled:
+            return
+
+        rows = self._convert_items(*items)
+        if len(items) == 0:
+            raise ValueError
+        elif len(rows) == 1:
+            row = rows[0]
+            logger.debug(f'Writing into "{self._worksheet.spreadsheet.title}/'
+                         f'{self._worksheet.title}":\n\t{row}')
+
+            self._write_row(row)
+            logger.info(f'Successfully writen row '
+                        f'into {self._worksheet_name()}')
+        else:
+            msg = f'Writing {len(rows)} rows into ' \
+                  f'"{self._worksheet.spreadsheet.title}/' \
+                  f'{self._worksheet.title}":'
+            for row in rows:
+                msg += f'\n\t{row}'
+            logger.debug(msg)
+
+            for row in rows:
+                self._write_row(row)
+            logger.info(f'Successfully writen {len(rows)} rows '
+                        f'into {self._worksheet_name()}')
+
+    def _write_row(self, row: tuple):
+        self._worksheet.append_row(row)
+
+    def _convert_items(self, *items) -> Tuple[tuple, ...]:
+        return tuple(self.Row.to_tuple(item=item) for item in items)
+
+    def _worksheet_name(self):
+        return f'"{self._worksheet.spreadsheet.title}"/"{self._worksheet.title}"'
+
+
+class GSpreadWriter(BaseGSpreadWriter):
+
+    @property
+    def Row(self):
+        return GSpreadRow
+
+
+class BackupGSpreadWriter(BaseGSpreadWriter):
+
+    @property
+    def Row(self):
+        return BackupGSpreadRow
