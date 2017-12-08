5ae9e9a35a0698c3dac142fe3c10d191d37a8220
Illia Ananich <illia.ananich@gmail.com>
2017-12-08 18:10:02 +0200

    fix typo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     license='MIT',
     author='Illia Ananich',
     author_email='illia.ananich@gmail.com',
-    packages=find_packages('src'),
+    packages=find_packages('scrapy_ntk'),
     install_requires=[
         'scrapinghub>=2.0.0',
         'Scrapy>=1.4.0',
